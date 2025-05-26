76d4552aad9cf39a1936a29c3ed753707900dac6
Vladislav882-sys <vladlord2005@gmail.com>
2025-05-26 18:12:56 +0300

    Update 2_hw.py

--- 2_hw.py
+++ 2_hw.py
@@ -1,1 +1,26 @@
+def task_1() -> None:
+    a: int = 11567
+    b: float = 1.25625625
+    c: str = "Первое задание"
+    d: list = [1, 2, 3]
+    e: bool = True
 
+    print(type(a))
+    print(type(b))
+    print(type(c))
+    print(type(d))
+    print(type(e))
+    
+task_1()
+
+def task_2() -> None:
+    a = [1, 2, 3, 5, 8, 13, 21]
+    print(a[:3])
+
+task_2()
+//последовательность фибоначчи
+
+def task_3(num: int) -> int:
+    return num ** 2
+
+print(task_3(5))
